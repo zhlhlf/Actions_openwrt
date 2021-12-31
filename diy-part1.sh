320c319bca93b3516daab4c9cc1cfe9ab2e81b2d
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2021-12-31 10:59:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,7 @@
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.1/192.168.50.5/g' /sdcard/66.txt
+
+# set default theme to openwrt2020
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/openwrt2020"' `find package -type f -path '*/default-settings/files/zzz-default-settings'`
+
+
