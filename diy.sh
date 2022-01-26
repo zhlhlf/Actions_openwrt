0155294caa488e50da8d80fe78dd1a4989250ecb
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2022-01-26 14:39:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 #      zzz
 # sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/netgear"' `find package -type f -path '*/default-settings/files/zzz-default-settings'`
 
-# sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/netgear"' `find package -type f -path '*/default-settings/files/99-default-settings'`
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/netgear"' `find package -type f -path '*/default-settings/files/99-default-settings'`
 
 
