c67e82ae927e2ea2a5f864f113aa3559874de18e
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2022-01-26 09:17:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,7 +2,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# set default theme to openwrt2020
-sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/netgear"' `find package -type f -path '*/default-settings/files/zzz-default-settings'`
+# set default theme 
+#      zzz
+#sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/netgear"' `find package -type f -path '*/default-settings/files/zzz-default-settings'`
 
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/netgear"' `find package -type f -path '*/default-settings/files/999-default-settings'`
 
