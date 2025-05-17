362b2d37223c93b0048fb587363d18bb4cfc978b
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2025-05-18 01:45:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,9 +3,4 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # set default theme 
-#      zzz
-# sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/netgear"' `find package -type f -path '*/default-settings/files/zzz-default-settings'`
-
-# sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/netgear"' `find package -type f -path '*/default-settings/files/99-default-settings'`
-
-
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
